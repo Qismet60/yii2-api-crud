ebcb51202bfc202ab3eb584a6e0b2a163f4fe1b9
Qismət Hüseynov <50789493+Qismet60@users.noreply.github.com>
2020-11-07 23:04:15 +0200

    update Post Controller

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -3,6 +3,9 @@
 namespace app\controllers;
 
 use app\models\Post;
+use phpDocumentor\Reflection\Types\This;
+use yii\db\Exception;
+use yii\web\NotFoundHttpException;
 
 class PostController extends \yii\web\Controller
 {
@@ -15,9 +18,25 @@ class PostController extends \yii\web\Controller
     public function actionCreate()
     {
         $query = new Post();
-        if ($query->load(\Yii::$app->request->post()) && $query->validate())
+        if ($query->load(\Yii::$app->request->post()) && $query->save())
         {
-            
+            return $this->redirect(['view','id'=>$query->id]);
+        }else{
+            return $this->render('index',[
+               'model' =>$query
+            ]);
+        }
+    }
+    public function actionView($id)
+    {
+        $model = Post::findOne($id);
+        if ($model===null){
+            throw new NotFoundHttpException();
+        }
+        else{
+            return $this->render('view',[
+               'model' => $model
+            ]);
         }
     }
 
